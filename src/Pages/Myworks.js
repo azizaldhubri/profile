d6cdd012dfd5131a6f6a803b1bd7c24b57e46c87
azizaldhubri <azizaldhubri@gmail.com>
2024-12-30 01:52:33 +0300

    Update Myworks.js

--- src/Pages/Myworks.js
+++ src/Pages/Myworks.js
@@ -101,8 +101,8 @@ export default function Myworks(){
                                
                 </div>
 
-                <div className='px-3 py-3 col-lg-6 col-md-12 col-sm-12 col-12  border  '>
-                     <p className='text-end '>  بناء مشروع عملي لمهام الموظفين ولكن لم اعرض رابط المشروع نظرا لحقوق الملكية 
+                <div className='px-3 py-3 col-lg-6 col-md-12 col-sm-12 col-12    '>
+                     <p className='text-end border'>  بناء مشروع عملي لمهام الموظفين ولكن لم اعرض رابط المشروع نظرا لحقوق الملكية 
                    لدى الشركة التي تستخدم الموقع
                   </p>  
                                     
